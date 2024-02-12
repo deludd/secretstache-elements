9ac5a0ed8798209db032a6c5475cb076af69631d
deludd <adimitrov2000@gmail.com>
2024-02-12 17:02:33 +0200

    Move index outside the src

---
+++ index.js
@@ -0,0 +1,4 @@
+export { default as BCImageRenderer } from './src/blocks/BCImageRenderer';
+export { default as BCImagePicker } from './src/blocks/BCImagePicker';
+export { default as SvgImageHandler } from './src/blocks/SvgImageHandler';
+export { default as cleanSvgString } from './src/utils/cleanSvgString';
--- src/index.js
+++
@@ -1,4 +0,0 @@
-export { default as BCImageRenderer } from './blocks/BCImageRenderer';
-export { default as BCImagePicker } from './blocks/BCImagePicker';
-export { default as SvgImageHandler } from './blocks/SvgImageHandler';
-export { default as cleanSvgString } from './utils/cleanSvgString';
